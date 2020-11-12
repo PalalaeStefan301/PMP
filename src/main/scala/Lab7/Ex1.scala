d4b2928cc0ced5f60d4624d1a24c585e130a7e1b
Stefan Palalae <palalaestefan301@gmail.com>
2020-11-12 11:16:02 +0200

    Revert "Lab 6 added"
    This reverts commit 3a63062ffc8cb611aa77fcc1b2a4d4ff4f985ea2.

--- src/main/scala/Lab7/Ex1.scala
+++ src/main/scala/Lab7/Ex1.scala
@@ -4,28 +4,12 @@ import com.cra.figaro.language._
 import com.cra.figaro.algorithm.sampling._
 
 object Ex1 {
-	val s = 7/13
-	val par = Map(s/8.00 -> "Par-2", s/2 -> "Par-1", s -> "Par",(4/5)*(1-((13*s)/8)) -> "Par+1", (1/5)*(1-((13*s)/8)) -> "Par+2")
-	val holes = List(3,4,5,3,5,4,3,3,4,5,5,4,3,5,4,4,5,5)
-	val targets = holes ::: data.keys.toList
-	val golf_holes = generate(targets)
-	for { (chance, par_value) <- data } {
-			golf_holes(chance).observe(par_value)
-	}
-
 	def main(args: Array[String]) {
-		val queryElements = holes.map(golf_holes(_))
-		val queryTargets = golfPrior :: golfChangeRate :: queryElements
-		val algorithm = VariableElimination(queryTargets:_*)
+		val test = Constant("Test")
+
+		val algorithm = Importance(1000, test)
 		algorithm.start()
-		for { hole <- holes } {
-			println("Probability to get the hole " + hole + " = " +
-			algorithm.probability(golf_holes(hole), "Par"))
-		}
-		println("Expected prior probability of par = " +
-		algorithm.mean(golfPrior))
-		println("Expected par change rate = " +
-		algorithm.mean(golfChangeRate))
-		algorithm.kill()
+		
+		println(algorithm.probability(test, "Test"))
 	}
 }
