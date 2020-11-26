076bc0c3c9acb74bcf3bb3a62d623e73c1c008c6
Stefan Palalae <palalaestefan301@gmail.com>
2020-11-26 11:03:54 +0200

    Lab 8 added

--- src/main/scala/Lab8/Ex1.scala
+++ src/main/scala/Lab8/Ex1.scala
@@ -4,8 +4,28 @@ import com.cra.figaro.language._
 import com.cra.figaro.algorithm.sampling._
 
 object Ex1 {
-	class Research {
-		val  
+	abstract class Department (name: String) {
+
+	}
+	class Research_and_Development(name: String) extends Department(name){
+		val quality = Uniform(0, 1)
+	}
+	class Production(name: String) extends Department(name){
+		val comunication_with_sales = Uniform(0,1)
+		val quality = Uniform(0, 1)
+	}
+	class Sales(name: String) extends Department(name){
+		val nr_prod_sold
+		val quality = Uniform(0, 1)
+	}
+	class Human_resources(name: String) extends Department(name){
+		val quality = Uniform(0, 1)
+	}
+	class Finance(name: String) extends Department(name){
+		val quality = Uniform(0, 1)
+	}
+	case class Company(res_dev: Research_and_Development, production: Production, sales: Sales, hr: Human_resources, finance: Finance){
+		
 	}
 	def main(args: Array[String]) {
 		val test = Constant("Test")
@@ -16,4 +36,3 @@ object Ex1 {
 		println(algorithm.probability(test, "Test"))
 	}
 }
-//: Research and Development, Production, Sales,Human Resources, and Finance
