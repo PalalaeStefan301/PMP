e6f5c5e5fac85ee6bd23b74613251c99f613346a
Stefan Palalae <palalaestefan301@gmail.com>
2020-12-03 11:34:18 +0200

    lab 9 added

--- src/main/scala/Lab9/Ex1.scala
+++ src/main/scala/Lab9/Ex1.scala
@@ -2,14 +2,39 @@ package Lab9
 
 import com.cra.figaro.language._
 import com.cra.figaro.algorithm.sampling._
+import com.cra.figaro.algorithm.filtering.ParticleFilter
 
 object Ex1 {
+	def transition(learned: Int,difficulty: Int):
+ 		(Element[(Int, Int, Int)]) = {
+ 		
+	}
+	def nextUniverse(previous: Universe): Universe = {
+ 		val next = Universe.createNew()
+ 		val previousSeated = previous.get[List[Int]]("seated")
+ 		val previousWaiting = previous.get[Int]("waiting")
+ 		val state = Chain(previousSeated, previousWaiting, transition _)
+		Apply(state, (s: (Int, Int, Int)) => s._1)("seated", next)
+ 		Apply(state, (s: (Int, Int, Int)) => s._2)("waiting", next)
+ 		Apply(state, (s: (Int, Int, Int)) => s._3)("arriving", next)
+ 		next
+}
+	}
 	def main(args: Array[String]) {
-		val test = Constant("Test")
-
-		val algorithm = Importance(1000, test)
-		algorithm.start()
-		
-		println(algorithm.probability(test, "Test"))
+		val arrivingObservation = List(Some(1),Some(1),Some(1), None, None, None, None, None, None, None, None, None)
+		val alg = ParticleFilter(initial,nextUniverse,100)
+		alg.start()
+		for(time <- 1 to 10)
+		{
+			val evidence = {
+				arrivingObservation(time) match {
+					case None -> List()
+					case Some(n) -> List(NamedEvidence("score",Observation(n)))
+				}
+			}
+			alg.advanceTime(evidence)
+			print("Chapter " + time + ":")
+			println("exptected learning = " + alg.currentExpectation("score",(i : Int)=>i))
+		}
 	}
 }
